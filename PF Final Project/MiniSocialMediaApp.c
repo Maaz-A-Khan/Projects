7f2c0de2619aea7e633f7b0f3723d80e81f73587
Maaz Khan <maaz.ak211@gmail.com>
2024-11-17 15:35:46 +0500

    main function
    Added the main function which will call other functions

--- PF Final Project/MiniSocialMediaApp.c
+++ PF Final Project/MiniSocialMediaApp.c
@@ -10,3 +10,43 @@ char currentUser[20];
 typedef struct{
 	char user[20] , time[30], category[30], post[251];
 }post;
+
+int main(void){
+    int choice;
+
+    while (run==1){
+        if (loggedIn==1){
+            postMenu();
+        }else{
+            printf("------------------------\n");
+            printf("---Welcome to Tweet---");
+            printf("\n------------------------\n\n");
+            printf("-Login      = 1\n");
+            printf("-Register   = 2\n");
+            printf("\n\n-To exit    =0\n");
+            printf("\nChoose an option:");
+            scanf("%d",&choice);
+            getchar();
+            
+            switch (choice){
+            case 1:
+                login();
+                break;
+            
+            case 2:
+                registeration();
+                break;
+
+            case 0:
+                printf("Good Bye !");
+                run=0;
+                break;
+
+            default:
+                printf("Invalid option");
+                break;
+            }
+        }
+    }
+
+}
